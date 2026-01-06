9e86cd5b9eb07be10c9ec3b3882d533747d0e21c
Pavel <mr.dumbmoose@gmail.com>
2026-01-06 11:12:45 +0300

    Add --version flag to CLI

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/student/go-cli-tool/internal/service"
 )
 
 func main() {
+	showVersion := flag.Bool("version", false, "show version")
+	flag.Parse()
+
+	if *showVersion {
+		fmt.Println("go-cli-tool", service.Version())
+		return
+	}
 	service.PrintStartMessage()
 	fmt.Println("version", service.Version())
 }
