c5755e89c5ef67dcad0ccf7b46db5b6810e7ca91
Pavel <mr.dumbmoose@gmail.com>
2026-01-08 10:15:07 +0300

    Fix help output newlines

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,10 +13,10 @@ func main() {
 	flag.Usage = func() {
 		fmt.Fprintf(os.Stderr, "go-cli-tool %s\n\n", service.Version())
 		fmt.Fprintln(os.Stderr, "Usage:")
-		fmt.Fprintln(os.Stderr, "  go-cli-tool [options]\n")
+		fmt.Fprintln(os.Stderr, "  go-cli-tool [options]")
 		fmt.Fprintln(os.Stderr, "Options:")
 		flag.PrintDefaults()
-		fmt.Fprintln(os.Stderr, "\nExamples:")
+		fmt.Fprintln(os.Stderr, "Examples:")
 		fmt.Fprintln(os.Stderr, "  go run ./cmd/app")
 		fmt.Fprintln(os.Stderr, "  go run ./cmd/app --version")
 		fmt.Fprintln(os.Stderr, "  go run ./cmd/app --help")
