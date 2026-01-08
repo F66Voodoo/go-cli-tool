f1e141e949b7a9462fc576e60a2c7293cec2d477
Pavel <mr.dumbmoose@gmail.com>
2026-01-08 10:03:49 +0300

    Add cutom CLI help

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -3,11 +3,25 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 
 	"github.com/student/go-cli-tool/internal/service"
 )
 
 func main() {
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "go-cli-tool %s\n\n", service.Version())
+		fmt.Fprintln(os.Stderr, "Usage:")
+		fmt.Fprintln(os.Stderr, "  go-cli-tool [options]\n")
+		fmt.Fprintln(os.Stderr, "Options:")
+		flag.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "\nExamples:")
+		fmt.Fprintln(os.Stderr, "  go run ./cmd/app")
+		fmt.Fprintln(os.Stderr, "  go run ./cmd/app --version")
+		fmt.Fprintln(os.Stderr, "  go run ./cmd/app --help")
+	}
+
 	showVersion := flag.Bool("version", false, "show version")
 	flag.Parse()
 
